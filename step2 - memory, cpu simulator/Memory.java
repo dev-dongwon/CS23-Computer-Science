c67b0697bf05b73d42fa54a57baa820ce6ff468d
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-09 02:03:35 +0900

    word 단위로 peek 할 수 있께 작동 단위 수정

--- step2 - memory, cpu simulator/Memory.java
+++ step2 - memory, cpu simulator/Memory.java
@@ -2,29 +2,23 @@ package step2MemoryCpuSimule;
 
 public class Memory {
 
-	private static boolean[] BYTES = new boolean[8];
 	private static boolean[] WORD = new boolean[16];
-	private static boolean[] PROGRAM_TEXT = new boolean[(int) (Math.pow(2, 16) * WORD.length)];
-	private static boolean[] PROGRAM_HEAP = new boolean[(int) (Math.pow(2, 16) * WORD.length)];
+	private static boolean[][] PROGRAM_TEXT = new boolean[(int) Math.pow(2, 16)][WORD.length];
+	private static boolean[][] PROGRAM_HEAP = new boolean[(int) Math.pow(2, 16)][WORD.length];
 	
-	
-	public String peek(int address) {
-
+	public boolean[] peek(int address) {
+		
 		if (address >= 0x0000 && address <= 0xFFFF) {
 			for (int i=0; i<PROGRAM_TEXT.length; i+=WORD.length) {
-				for (int j=i; j<16; j++) {
-					if (j == address) {
-						return Integer.toHexString(i);
-					}
+				if (address >=i && address < i+WORD.length) {						
+					return PROGRAM_TEXT[i];
 				}
 			}
 			
 		} else if (address >= 0x10000 && address <= 0x1FFFF) {
 			for (int i=0; i<PROGRAM_HEAP.length; i+=WORD.length) {
-				for (int j=i; j<16; j++) {
-					if (j == address) {
-						return Integer.toHexString(i);
-					}
+				if (address >=i && address < i+WORD.length) {						
+					return PROGRAM_HEAP[i];
 				}
 			}
 			
