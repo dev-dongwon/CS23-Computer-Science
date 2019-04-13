e84e001f1e56fc9254e531603ac96d4b4078c7ff
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-13 16:57:39 +0900

    memory peek 메서드 인자값 String binary로 변경
    기존 String 32bitAddress 에서 binary 형식으로 파라미터값 변경

--- step2 - memory, cpu simulator/Memory.java
+++ step2 - memory, cpu simulator/Memory.java
@@ -1,48 +1,55 @@
-package step2MemoryCpuSimule;
+package step2ComputerSimulator;
+
+import java.util.Arrays;
 
 public class Memory {
+	
+	// 데이터 형식은 모두 binary로 통일
 
-	private static boolean[] WORD = new boolean[16];
-	private static boolean[][] PROGRAM_TEXT = new boolean[(int) Math.pow(2, 16)][WORD.length];
-	private static boolean[][] PROGRAM_HEAP = new boolean[(int) Math.pow(2, 16)][WORD.length];
+	private int[] WORD = new int[16];
+	private int[][] PROGRAM_TEXT = new int[(int) Math.pow(2, 16)][WORD.length];
+	private int[][] PROGRAM_HEAP = new int[(int) Math.pow(2, 16)][WORD.length];
+	private int[][][] MEMORY_MODEL = {this.PROGRAM_TEXT, this.PROGRAM_HEAP};
 	
-	public boolean[] peek(int address) {
+	public int[][][] getMEMORY_MODEL() {
+		return MEMORY_MODEL;
+	}
+
+	public int[] peek(String address32) {
 		
-		if (address >= 0x0000 && address <= 0xFFFF) {
-			for (int i=0; i<PROGRAM_TEXT.length; i+=WORD.length) {
-				if (address >=i && address < i+WORD.length) {						
-					return PROGRAM_TEXT[i];
-				}
-			}
-			
-		} else if (address >= 0x10000 && address <= 0x1FFFF) {
-			for (int i=0; i<PROGRAM_HEAP.length; i+=WORD.length) {
-				if (address >=i && address < i+WORD.length) {						
-					return PROGRAM_HEAP[i];
-				}
-			}
-			
+		Integer value = Integer.valueOf(address32, 2);
+		
+		if (value >= 0x0000 && value <= 0xFFFF) {
+			return MEMORY_MODEL[0][value];
+		} else if (value >= 0x10000 && value <= 0x1FFFF) {
+			return MEMORY_MODEL[1][value-0x10000];
 		} else {
-			Exception outOfRangeException = new Exception("out Of Range Exception");
-			
-			try {
-				throw outOfRangeException;
-			} catch (Exception e) {
-				System.out.println("메모리 범위를 초과하였습니다");
-				e.printStackTrace();
-			}
-			
-			System.out.println("시스템을 종료합니다");
+			RuntimeException OutOfRangeException = new RuntimeException("메모리 범위를 초과했습니다");
+			throw OutOfRangeException;
 		}
-		
-		return null;
-		
 	}
 	
-	public static void main(String[] args) {
+	public void locate(int[][] program) {
 		
-		Memory memory = new Memory();
-		System.out.println(memory.peek(4));
+		int[] emptyArea = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		int programTextAreaIndex = 0;
+		
+		for (int i=0; i<program.length; i++) {
+			if (Arrays.equals(emptyArea, MEMORY_MODEL[0][programTextAreaIndex])) {
+				MEMORY_MODEL[0][programTextAreaIndex++] = program[i];
+			}
+		}
 	}
 	
+	public int[] fetch(int program_count) {
+		return PROGRAM_TEXT[program_count];
+	}
+	
+	public int[] load(int address) {
+		return MEMORY_MODEL[1][address];
+	}
+	
+	public void store(int address, int[] data) {
+		MEMORY_MODEL[1][address] = data;
+	}
 }
