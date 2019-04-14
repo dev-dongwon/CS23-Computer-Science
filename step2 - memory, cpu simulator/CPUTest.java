465c497756f7d46fcdce0a99c55690fa6a5a4c11
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-14 12:33:25 +0900

    cpu and 테스트 케이스 추가 및 동작 확인

--- step2 - memory, cpu simulator/CPUTest.java
+++ step2 - memory, cpu simulator/CPUTest.java
@@ -10,10 +10,7 @@ public class CPUTest {
 	@Test
 	public void loadTest() {
 		Memory memory = new Memory();
-		ALU ALU = new ALU();
-		Instruction instruction = new Instruction();
-		Register register = new Register();
-		CPU cpu = new CPU(memory, register, ALU, instruction);
+		CPU cpu = new CPU(memory);
 		
 		int[] baseReg = {0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0};
 		int[] offsetReg = {0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0};
@@ -34,7 +31,62 @@ public class CPUTest {
 	
 	@Test
 	public void storeTest() {
+		Memory memory = new Memory();
+		CPU cpu = new CPU(memory);
 		
+		int[] baseReg = {0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0};
+		int[] offsetReg = {0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0};
+		
+		int[] data = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+
+//		STORE R5, R3, R4 ==> 0011 101 011 000 100 => R5 값을 R3 + R4 메모리 어드레스에 저장
+		int[] instructionBit = {0,0,1,1,1,0,1,0,1,1,0,0,0,1,0,0};
+		
+		cpu.register.R3 = baseReg;
+		cpu.register.R4 = offsetReg;
+		cpu.register.R5 = data;
+		
+		cpu.instruction.STORE(cpu, instructionBit);
+		assertArrayEquals(data, cpu.memory.getMEMORY_MODEL()[1][30]);
 	}
+	
+	@Test
+	public void andTest() {
+		Memory memory = new Memory();
+		CPU cpu = new CPU(memory);
+		
+		int[] case1 = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+		int[] case2 = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+		int[] expectedResult = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+		
+//		AND R3, R1, R6 ==> 0101 011 001 000 110 => R3 = R1 && R6 
+		cpu.register.R1 = case1;
+		cpu.register.R6 = case2;
+		
+		int[] instructionBit = {0,1,0,1,0,1,1,0,0,1,0,0,0,1,1,0};
+		cpu.instruction.AND(cpu, instructionBit);
+		
+		assertArrayEquals(expectedResult, cpu.register.R3);
+	}
+	
+//	@Test
+//	public void orTest() {
+//		Memory memory = new Memory();
+//		CPU cpu = new CPU(memory);
+//		
+//		int[] case1 = {1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+//		int[] case2 = {0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1};
+//		int[] expectedResult = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+//		
+////		AND R3, R1, R6 ==> 0101 011 001 000 110 => R3 = R1 && R6 
+//		cpu.register.R1 = case1;
+//		cpu.register.R6 = case2;
+//		
+//		int[] instructionBit = {0,1,0,1,0,1,1,0,0,1,0,0,0,1,1,0};
+//		cpu.instruction.AND(cpu, instructionBit);
+//		
+//		assertArrayEquals(expectedResult, cpu.register.R3);
+//
+//	}
 
 }
