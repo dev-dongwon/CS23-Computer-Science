524119e70c11cb256089e378b2f2513ec726a44d
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-14 17:08:38 +0900

    register map 구현 제거

--- step2 - memory, cpu simulator/Register.java
+++ step2 - memory, cpu simulator/Register.java
@@ -1,8 +1,5 @@
 package step2ComputerSimulator;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Register {
 	
 	public Register() {
@@ -14,7 +11,6 @@ public class Register {
 		this.R5 = new int[16];;
 		this.R6 = new int[16];;
 		this.R7 = new int[16];;
-		this.registers = new HashMap<>();
 	}
 	
 	public int PC;
@@ -24,19 +20,5 @@ public class Register {
 	public int[] R4;
 	public int[] R5;
 	public int[] R6;
-	public int[] R7;
-	
-	Map<Integer, int[]> registers;
-
-	public Map<Integer, int[]> getRegisters() {
-		registers.put(1, this.R1);
-		registers.put(2, this.R2);
-		registers.put(3, this.R3);
-		registers.put(4, this.R4);
-		registers.put(5, this.R5);
-		registers.put(6, this.R6);
-		registers.put(7, this.R7);
-		return registers;
-	}
-	
+	public int[] R7;	
 }
