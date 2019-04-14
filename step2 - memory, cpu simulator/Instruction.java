57e1d62ed11a379bd20dfd3a94550cf474a1e8ee
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-14 11:36:43 +0900

    CPU 명령어 Instruction 객체 생성 및 LOAD, STORE 메서드 추가
    instrcution 객체 생성 후 isntruction 명령어 추가
    LOAD, STORE 동작 구현

---
+++ step2 - memory, cpu simulator/Instruction.java
@@ -0,0 +1,83 @@
+package step2ComputerSimulator;
+
+
+public class Instruction {
+	
+	public void LOAD(CPU cpu, int[] IR) {
+		int[] dstRegBit = {IR[4], IR[5], IR[6]};
+		int[] baseRegBit = {IR[7], IR[8], IR[9]};
+		int[] baseReg = cpu.decoder(cpu.getRegisterNumber(baseRegBit));
+		
+		String offsetRegBinary = "";
+
+		if (IR[10] == 0) {
+			int[] offsetRegBit = {IR[13], IR[14], IR[15]};
+			int[] offsetReg = cpu.decoder(cpu.getRegisterNumber(offsetRegBit));
+			offsetRegBinary = cpu.BinaryArrayToString(offsetReg);
+		} else {
+			int[] offsetValue = {IR[11], IR[12], IR[13], IR[14], IR[15]};
+			offsetRegBinary = cpu.BinaryArrayToString(offsetValue);
+		}
+		
+		String baseRegBinary = cpu.BinaryArrayToString(baseReg);
+		Integer address = cpu.ALU.ADD(Integer.valueOf(baseRegBinary, 2), Integer.valueOf(offsetRegBinary, 2));
+		int[] result = cpu.memory.peek(Integer.toBinaryString(address));
+
+		cpu.decoder(cpu.getRegisterNumber(dstRegBit), result);
+	}
+	
+	
+	public void STORE(CPU cpu, int[] IR ) {
+		int[] dstRegBit = {IR[4], IR[5], IR[6]};
+		int[] dstRegData = cpu.decoder(cpu.getRegisterNumber(dstRegBit));
+		
+		int[] baseRegBit = {IR[7], IR[8], IR[9]};
+		int[] baseRegData = cpu.decoder(cpu.getRegisterNumber(baseRegBit));
+
+		String baseRegBinary = cpu.BinaryArrayToString(baseRegData);
+		
+		String offsetRegBinary = "";
+
+		if (IR[10] == 0) {
+			int[] offsetRegBit = {IR[13], IR[14], IR[15]};
+			int[] offsetReg = cpu.decoder(cpu.getRegisterNumber(offsetRegBit));
+			offsetRegBinary = cpu.BinaryArrayToString(offsetReg);
+		} else {
+			int[] offsetValue = {IR[11], IR[12], IR[13], IR[14], IR[15]};
+			offsetRegBinary = cpu.BinaryArrayToString(offsetValue);
+		}
+		
+		Integer address = cpu.ALU.ADD(Integer.valueOf(baseRegBinary, 2), Integer.valueOf(offsetRegBinary, 2));
+		cpu.memory.store(address, dstRegData);
+	}
+	
+//	public void AND(int[] dstReg, int[] opReg1, int[] opReg2) {
+//		dstReg = this.ALU.AND(opReg1, opReg2);
+//	}
+//	
+//	public void OR(int[] dstReg, int[] opReg1, int[] opReg2) {
+//		dstReg = this.ALU.OR(opReg1, opReg2);
+//	}
+//	
+//	public void ADD(int[] dstReg, int[] opReg1, int[] opReg2) {
+//		String baseRegBinary = BinaryArrayToString(opReg1);
+//		String offsetRegBinary = BinaryArrayToString(opReg2);
+//		
+//		Integer value = ALU.ADD(Integer.valueOf(baseRegBinary, 2), Integer.valueOf(offsetRegBinary, 2));
+//		dstReg = StringTo16bitArr(Integer.toBinaryString(value));
+//	}
+//	
+//	
+//	public void SUB(int[] dstReg, int[] opReg1, int[] opReg2) {
+//		String opReg1Binary = BinaryArrayToString(opReg1);
+//		String opReg2Binary = BinaryArrayToString(opReg2);
+//		
+//		Integer value = ALU.SUB(Integer.valueOf(opReg1Binary, 2), Integer.valueOf(opReg2Binary, 2));
+//		dstReg = StringTo16bitArr(Integer.toBinaryString(value));
+//	}
+	
+
+
+
+	
+}
