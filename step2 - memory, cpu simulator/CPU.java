d9a22f54eca30a51e1b002d7fbd827f561c1bc6a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-04-14 17:15:34 +0900

    cpu 각 메서드 줄바꿈으로 가독성 향상

--- step2 - memory, cpu simulator/CPU.java
+++ step2 - memory, cpu simulator/CPU.java
@@ -18,35 +18,6 @@ public class CPU {
 		this.ALU = ALU;
 	}
 
-	public String BinaryArrayToString(int[] binaryArr) {
-		String binaryStr = "";
-		for (int i=0; i<binaryArr.length; i++) {
-			binaryStr += binaryArr[i];
-		}
-
-		return binaryStr;
-	}
-	
-	public int[] StringTo16bitArr(String binaryStr) {
-		int[] result = new int[16];
-		for (int i=0; i<binaryStr.length(); i++) {
-			result[i] = Character.getNumericValue(binaryStr.charAt(i));
-		}
-		
-		return result;
-	}
-	
-	public int getRegisterNumber(int[] registerBit) {
-		int key = 0;
-		int Num2bit = 1;
-		
-		for (int i=registerBit.length-1; i>=0; i--) {
-			key += registerBit[i] * Num2bit;
-			Num2bit *= 2;
-		}
-		return key;
-	}
-	
 	public void reset() {
 		Register register = new Register();
 		this.register = register;
@@ -54,34 +25,12 @@ public class CPU {
 	
 	public int[] fetch() {
 		this.register.PC += 1;
-		return this.memory.fetch(this.register.PC);
+		return this.memory.fetch(this.register.PC-1);
 	}
 	
-	public Register[] dump() {
-		Register[] registers = {this.register};
-		return registers;
-	}
-	
-	
-	public void decoder(int bit, int[] data) {
-		if (bit == 1) this.register.R1 = data;
-		else if (bit == 2) this.register.R2 = data;
-		else if (bit == 3) this.register.R3 = data;
-		else if (bit == 4) this.register.R4 = data;
-		else if (bit == 5) this.register.R5 = data;
-		else if (bit == 6) this.register.R6 = data;
-		else if (bit == 7) this.register.R7 = data;
-	}
-
-	public int[] decoder(int bit) {
-		if (bit == 1) return this.register.R1;
-		else if (bit == 2) return this.register.R2;
-		else if (bit == 3) return this.register.R3;
-		else if (bit == 4) return this.register.R4;
-		else if (bit == 5) return this.register.R5;
-		else if (bit == 6) return this.register.R6;
-		else if (bit == 7) return this.register.R7;
-		return null;
+	public int[][] dump() {
+		int[][] registerArr = {this.register.R1, this.register.R2, this.register.R3, this.register.R4, this.register.R5, this.register.R6, this.register.R7};
+		return registerArr;
 	}
 	
 	public void execute(int[] IR) {
@@ -112,23 +61,73 @@ public class CPU {
 		case 6:
 			instruction.OR(this, IR);
 			break;
-//		case 7:
-//			instruction.ADD(this, IR);
-//			break;
-//		case 8:
-//			instruction.ADD(this, IR);
-//			break;
-//		case 9:
-//			instruction.SUB(this, IR);
-//			break;
-//		case 10:
-//			instruction.SUB(this, IR);
-//			break;
-//		case 11:
-//			instruction.MOV(this, IR);
+		case 7:
+			instruction.ADD(this, IR);
+			break;
+		case 8:
+			instruction.ADD(this, IR);
+			break;
+		case 9:
+			instruction.SUB(this, IR);
+			break;
+		case 10:
+			instruction.SUB(this, IR);
+			break;
+		case 11:
+			instruction.MOV(this, IR);
 		default:
 			break;
 		}
 	}
 	
+	public void decoder(int bit, int[] data) {
+		if (bit == 1) this.register.R1 = data;
+		else if (bit == 2) this.register.R2 = data;
+		else if (bit == 3) this.register.R3 = data;
+		else if (bit == 4) this.register.R4 = data;
+		else if (bit == 5) this.register.R5 = data;
+		else if (bit == 6) this.register.R6 = data;
+		else if (bit == 7) this.register.R7 = data;
+	}
+
+	public int[] decoder(int bit) {
+		if (bit == 1) return this.register.R1;
+		else if (bit == 2) return this.register.R2;
+		else if (bit == 3) return this.register.R3;
+		else if (bit == 4) return this.register.R4;
+		else if (bit == 5) return this.register.R5;
+		else if (bit == 6) return this.register.R6;
+		else if (bit == 7) return this.register.R7;
+		return null;
+	}
+
+	public String BinaryArrayToString(int[] binaryArr) {
+		String binaryStr = "";
+		for (int i=0; i<binaryArr.length; i++) {
+			binaryStr += binaryArr[i];
+		}
+
+		return binaryStr;
+	}
+	
+	public int[] StringTo16bitArr(String binaryStr) {
+		int[] result = new int[16];
+		for (int i=binaryStr.length()-1; i>=0; i--) {
+			result[i] = Character.getNumericValue(binaryStr.charAt(i));
+		}
+		
+		return result;
+	}
+	
+	public int getRegisterNumber(int[] registerBit) {
+		int key = 0;
+		int Num2bit = 1;
+		
+		for (int i=registerBit.length-1; i>=0; i--) {
+			key += registerBit[i] * Num2bit;
+			Num2bit *= 2;
+		}
+		return key;
+	}
+
 }
